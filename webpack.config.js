78959750830f2e6c3ae13e267dd41db045c7f198
Vladislav <vladislavmalyy.dev@gmail.com>
2017-10-10 20:18:36 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    watch: true,
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!less-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
+};
